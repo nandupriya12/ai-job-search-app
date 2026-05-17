2277398d07e38b78e119ebc31c59eebce10bc437
Nandu Priya <nandupriya2006@gmail.com>
2026-05-17 22:56:05 +0530

    Final polished UI and India jobs

--- scraper.py
+++ scraper.py
@@ -10,40 +10,39 @@ APP_KEY = os.getenv("ADZUNA_APP_KEY")
 
 def get_jobs(job, location, experience):
 
-    url = url = "https://api.adzuna.com/v1/api/jobs/gb/search/1"
+    url = "https://api.adzuna.com/v1/api/jobs/in/search/1"
 
     params = {
         "app_id": APP_ID,
         "app_key": APP_KEY,
-        "results_per_page": 10,
+        "results_per_page": 20,
         "what": job,
-       "where": location.split(",")[0]
+        "where": location
     }
 
     try:
 
         response = requests.get(url, params=params)
 
-        print(response.status_code)
-        print(response.text)
-
         data = response.json()
 
         jobs = []
 
-        for item in data.get("results", []):
+        if "results" not in data:
+            return []
+
+        for item in data["results"]:
 
             jobs.append({
                 "title": item.get("title", "N/A"),
                 "company": item.get("company", {}).get("display_name", "N/A"),
                 "location": item.get("location", {}).get("display_name", "N/A"),
                 "description": item.get("description", ""),
-                "salary": item.get("salary_min", "Not Available"),
                 "redirect_url": item.get("redirect_url", "#")
             })
 
         return jobs
 
     except Exception as e:
-        print("ERROR:", e)
+        print(e)
         return []
